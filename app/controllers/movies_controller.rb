aab397bc00256e0019819ae817e0e6f04b4eb759
Brad Hankee <brad.hankee@gmail.com>
2017-12-05 12:00:50 -0500

    Added some styling to index page.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,7 +15,7 @@ class MoviesController < ApplicationController
     def update
         @movie = Movie.find(params[:id])
         if @movie.update(movie_params)
-          redirect_to @movie
+            redirect_to @movie, notice: "Movie successfully updated!"
         else
           render :edit
         end
@@ -28,7 +28,8 @@ class MoviesController < ApplicationController
     def create
         @movie = Movie.new(movie_params)
         if @movie.save
-            redirect_to @movie
+            redirect_to @movie, notice: "Movie successfully created!"
+            
         else
             render :new
         end
@@ -37,8 +38,8 @@ class MoviesController < ApplicationController
     def destroy
         @movie = Movie.find(params[:id])
         @movie.destroy
-        redirect_to movies_url
-    end
+        redirect_to movies_url, alert: "Movie successfully deleted!"
+      end
 end
 
 private
